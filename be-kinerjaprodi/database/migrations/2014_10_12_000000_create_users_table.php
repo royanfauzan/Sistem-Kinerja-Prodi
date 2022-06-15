0e2b96c2e1e5500cd88d7a9e9d17e3aa194bf43b
rona <litanarona@gmail.com>
2022-06-15 15:22:30 +0800

    buat model user dan profil dosen

--- be-kinerjaprodi/database/migrations/2014_10_12_000000_create_users_table.php
+++ be-kinerjaprodi/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id('NIDK');
             $table->string('password');
+            $table->string('role');
+            $table->smallInteger('level_akses');
+            
             $table->rememberToken();
             $table->timestamps();
         });

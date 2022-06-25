8d1e9d389cc8ad311deb170b3f88ad96a69fbf5d
EvaPramesti <dwikeva456@gmail.com>
2022-06-25 19:47:59 +0800

    push evaa

---
+++ be-kinerjaprodi/database/migrations/2022_06_24_060648_create_matkuls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatkulsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matkuls', function (Blueprint $table) {
+            $table->id();
+            $table->String('nama_matkul');
+            $table->Integer('sks');
+            $table->foreignId('prodi_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matkuls');
+    }
+}
--- be-kinerjaprodi/routes/api.php
+++ be-kinerjaprodi/routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatkulController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\KepuasanMHSController;
+use App\Http\Controllers\ProdukMHSController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//route prodi
+Route::post('Prodi', [ProdiController::class, 'store']);
+Route::put('Prodi/{id}', [ProdiController::class, 'update']);
+
+//route mahasiswa
+Route::post('Mahasiswa', [MahasiswaController::class, 'store']);
+Route::put('Mahasiswa/{id}', [MahasiswaController::class, 'update']);
+
+//route matkul
+Route::post('Matkul', [MatkulController::class, 'store']);
+Route::put('Matkul/{id}', [MatkulController::class, 'update']);
+
+//route kepuasan mahasiswa
+Route::post('KepuasanMHS', [KepuasanMHSController::class, 'store']);
+Route::put('KepuasanMHS/{id}', [KepuasanMHSController::class, 'update']);
+
+//route produk mahasiswa
+Route::post('ProdukMHS', [ProdukMHSController::class, 'store']);
+Route::put('ProdukMHS/{id}', [ProdukMHSController::class, 'update']);

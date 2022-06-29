5c695dfa025d05d47575ca400f02a325c93b39bd
EvaPramesti <dwikeva456@gmail.com>
2022-06-29 11:19:31 +0800

    pusheva

--- be-kinerjaprodi/routes/api.php
+++ be-kinerjaprodi/routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\CapKurikulumController;
+use App\Http\Controllers\IntegrasiController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatkulController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\KepuasanMHSController;
+use App\Http\Controllers\PenelitianController;
+use App\Http\Controllers\PKMController;
 use App\Http\Controllers\ProdukMHSController;
 
 /*
@@ -41,3 +45,19 @@ Route::put('KepuasanMHS/{id}', [KepuasanMHSController::class, 'update']);
 //route produk mahasiswa
 Route::post('ProdukMHS', [ProdukMHSController::class, 'store']);
 Route::put('ProdukMHS/{id}', [ProdukMHSController::class, 'update']);
+
+//route data capaian kurikulum
+Route::post('CapaianKurikulum', [CapKurikulumController::class, 'store']);
+Route::put('CapaianKurikulum/{id}', [CapKurikulumController::class, 'update']);
+
+//route data PKM
+Route::post('PKM', [PKMController::class, 'store']);
+Route::put('PKM/{id}', [PKMController::class, 'update']);
+
+//route penelitian
+Route::post('Penelitian', [PenelitianController::class, 'store']);
+Route::put('Penelitian/{id}', [PenelitianController::class, 'update']);
+
+//route integrasi
+Route::post('Integrasi', [IntegrasiController::class, 'store']);
+Route::put('Integrasi/{id}', [IntegrasiController::class, 'update']);

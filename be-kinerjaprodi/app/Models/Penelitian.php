9c4d7aa38c9a3dfaa88fe2e6b3adcdbc7688fc3d
royanfauzan <royanfauzan7@gmail.com>
2022-08-01 22:23:05 +0800

    Penambahan Export EWMP dan Relasi penelitian dosen

--- be-kinerjaprodi/app/Models/Penelitian.php
+++ be-kinerjaprodi/app/Models/Penelitian.php
@@ -9,4 +9,9 @@ class Penelitian extends Model
 {
     use HasFactory;
     protected $guarded = ["id"];
+
+    public function anggotaDosens()
+    {
+        return $this->belongsToMany(profilDosen::class,'relasi_dos_pens','penelitian_id','profil_dosen_id')->withPivot('keanggotaan');
+    }
 }

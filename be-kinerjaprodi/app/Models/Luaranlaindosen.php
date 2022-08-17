9c28e9a2dc5e8152e60db4ddc1521076aac3f909
royanfauzan <royanfauzan7@gmail.com>
2022-08-17 13:54:08 +0800

    add luaran lain

---
+++ be-kinerjaprodi/app/Models/Luaranlaindosen.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Luaranlaindosen extends Model
+{
+    use HasFactory;
+    protected $guarded = ["id"];
+
+    public function anggotaDosens()
+    {
+        return $this->belongsToMany(profilDosen::class, 'rel_luar_dos', 'luaranlaindosen_id', 'profil_dosen_id')->withPivot('keanggotaan');
+    }
+}

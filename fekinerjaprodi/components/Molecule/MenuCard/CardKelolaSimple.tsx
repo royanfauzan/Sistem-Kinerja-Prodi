67d614bb5824a005d074d1b9bd979f1f9ab95abf
royanfauzan <royanfauzan7@gmail.com>
2022-08-22 21:05:34 +0800

    pembuatan card dashboard

---
+++ fekinerjaprodi/components/Molecule/MenuCard/CardKelolaSimple.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+interface KelolaCardProps {
+  judul: string;
+  keterangan: string;
+  halaman: string;
+  icon?: string;
+}
+
+export default function CardKelolaSimple(props: Partial<KelolaCardProps>) {
+  const { judul, keterangan, halaman } = props;
+  let icon = "bi bi-box";
+  if (props.icon) {
+    icon = props.icon;
+  }
+  return (
+    <div className="col-4 mt-3">
+      <div className="card">
+        <div className="card-body">
+          <div className="row mb-3">
+            <div className="col-9">
+            <h5 className="card-title">{judul}</h5>
+              <p className="card-text">{keterangan}</p>
+            </div>
+            <div className="col-3">
+              <div className="row">
+                {" "}
+                <i className={`${icon} fs-1 text-center`}></i>
+              </div>
+            </div>
+          </div>
+
+          <div className="row justify-content-center">
+            <a href={halaman} className="btn btn-primary mb-1">
+              Kelola Data
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
